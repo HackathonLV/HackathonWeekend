1a5cd49aeae99aca804501ceeac66f62573b0845
Andrew Carl <chuckles.theman@gmail.com>
2014-11-01 21:10:40 -0700

    restructured

--- ThruPay/ThruPay/wsgi.py
+++ ThruPay/ThruPay/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ExpressLane project.
+WSGI config for ThruPay project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ExpressLane.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ThruPay.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
